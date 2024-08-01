c0d7a8adeda3ad2f5fd52d0f5458fee79f583323
Muhammad Zawar <mzawar189@gmail.com>
2024-08-01 11:47:11 +0500

    Solve issue

--- src/components/Blogs/BlogPost.jsx
+++ src/components/Blogs/BlogPost.jsx
@@ -1,25 +1,3 @@
-// import arrow_right from "../../assets/arrow-right.svg";
-// import "bootstrap/dist/css/bootstrap.min.css";
-// import white_arrow from "../../assets/arrow-right-white.svg";
-
-// const BlogPost = ({ image, text, link }) => {
-//   return (
-//     <div className="col-12 col-md-6 col-lg-4 mb-4 d-flex align-items-stretch">
-//       <div className="card blog-post h-100 border-0 shadow-sm">
-//         <img src={image} className="card-img-top" alt="Blog Cover" />
-//         <div className="card-body d-flex flex-column justify-content-between align-items-center text-center">
-//           <p className="card-text text-grey fw-bold">{text}</p>
-//           <a href={link} className="btn btn-light text-green fw-semibold mt-3">
-//             Read More <img src={arrow_right} alt="Arrow" />
-//           </a>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default BlogPost;
-
 import arrow_right from "../../assets/arrow-right.svg";
 import "bootstrap/dist/css/bootstrap.min.css";
 //import "./Blogpost.css";
@@ -30,7 +8,7 @@ const BlogPost = ({ image, text, link }) => {
       <div className="card blog-post h-100 border-0 shadow-sm">
         <div className="card-img-wrapper">
           <img src={image} className="card-img-top" alt="Blog Cover" />
-          <div className="card-body d-flex flex-column justify-content-between align-items-center text-center position-absolute w-100 px-3 py-2">
+          <div className="card-body-blog d-flex flex-column justify-content-between align-items-center text-center position-absolute w-100 px-3 py-2">
             <p className="card-text text-grey fw-bold mb-2">{text}</p>
             <a href={link} className="btn btn-light text-green fw-semibold">
               Read More <img src={arrow_right} alt="Arrow" />
